de6ca7c824add3b48866e42b05ecc9451d0bcdbb
alisonliu999 <alison.liu999@gmail.com>
2022-09-29 21:46:30 -0700

    repeatString complete
    wrote and debugged a function to repeat a string multiple times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const times = Math.floor(Math.random()*1000);
+    function repeatString(string, times) {
+        if (times > 0) {
+            return string.repeat(times);
+        } else if (times == 0) {
+            return "";
+        } else {
+            return "ERROR";
+        }
+    }
+    repeatString("hey",);
 
 // Do not edit below this line
 module.exports = repeatString;
