f9acb2ad9fb27e7b708758f404bb499a9a509e68
alisonliu999 <alison.liu999@gmail.com>
2022-10-03 21:31:08 -0700

    sumAll solution
    wrote a function that uses an if statement to first render ERROR if it is not a number and not a negative, then uses variables to find the highest number to start there and increment until the starting number is less than or equal to the second number

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
+const numbers = [1, 2, 3, 4]
 const removeFromArray = function() {
-
+    numbers.splice(2,1);
+	return numbers;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function (x, y) {
+    if ((x > 0 && y > 0) && (typeof x === 'number' && typeof y === 'number')) {
+        var startx = x;
+        var starty = y;
+        var sum = 0;
+        if (x < y) {
+            for (var i = startx; i <= starty; i++) {
+                sum += i;
+            }
+            return sum;
+        } else if (x > y) {
+            for (var i = starty; i <= startx; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    } else {
+        return 'ERROR'
+     }
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
