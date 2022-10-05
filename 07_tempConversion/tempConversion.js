579b5416aa8822b8354ef85fa0504d0b946ae9a9
cloudylogs <alison.liu999@gmail.com>
2022-10-04 20:58:50 -0700

    tempConversion solution
    function includes formulas to convert to c to f and likewise, wrote a function with temperature as the parameter, defined the variable to include in the equation, used math.round to the 10s for rounding for one decimal point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  var fToCel = (temperature - 32) * 5/9;
+  var fToCelRounded = Math.round(fToCel * 10) / 10;
+  return fToCelRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  var cToFahr = (temperature * 9/5) + 32;
+  var cToFahrRounded = Math.round(cToFahr * 10) / 10;
+  return cToFahrRounded;
 };
 
 // Do not edit below this line
