65ec3c925fccfddf814f5e4562597ecb8b88a900
cloudylogs <alison.liu999@gmail.com>
2022-10-04 22:06:18 -0700

    calculator solution
    add, subtract, power were self explanatory
    sum was calculated with .reduce(), which took the current value and added it to the accumulator value (runs a reducer across all elements)
    multiply used a for statement with array length to run across the array in increments
    factorial used an if statement for the conditions of 0 and 1 and a nested for statement for the other factorials

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+  let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+  return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total = total * array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x < 0) {
+    return -1;
+  } else if (x === 0 || x === 1) {
+    return 1;
+  } else {
+    for (var i = x - 1; i >= 1; i--) {
+      x *= i;
+    }
+    return x;
+  }
 };
 
 // Do not edit below this line
