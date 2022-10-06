4b66aad727e13141146f07696c8819163d5bb62c
cloudylogs <alison.liu999@gmail.com>
2022-10-05 21:04:20 -0700

    palindromes solution
    created const of different variables to fit the step by step needed to determine how to check if something is a palindrome in the jest file
    changes it all to lowercase and removed special characters, turned it into an array and reversed it all then joined them back together to run a final if statement to see if the original and reversed strings match yay

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const lowerString = str.toLowerCase();
+    const string = lowerString.replace(/[^\w]|_/g,"");
+    const arrayValues = string.split('');
+    const reverseArrayValues = arrayValues.reverse();
+    const reverseString = reverseArrayValues.join('');
+    if (string == reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
