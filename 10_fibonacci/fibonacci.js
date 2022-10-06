6dfc0cb21b18c7aaaacf5e9d6f25c7dff5f69388
cloudylogs <alison.liu999@gmail.com>
2022-10-05 22:13:21 -0700

    fibonacci complete
    used a for statement to increment the fibonacci, wrote the constraints for if the numbers are negative and the strings

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    let temp;
 
+    if ( num == 1 || num == 2) return b;
+
+        for (let i = 2; i <= num; ++i) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return temp;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
