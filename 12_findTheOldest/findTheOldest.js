8c604507603ba565a2242f2819b00aa480a7cddd
cloudylogs <alison.liu999@gmail.com>
2022-10-10 22:00:56 -0700

    findTheOldest solution
    uses reduce to find the sum/difference of the values in the array to determine the oldest living/oldest person involved
    used chained arrays because we needed functions to get the age of the objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (oldestAge < currentAge) {
+            return currentPerson
+        } else {
+            return oldestPerson;
+        }
+    })
 };
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
